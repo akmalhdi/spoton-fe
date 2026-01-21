7f73eefc02df298dc5ea2959ad6c0bb858bcc995
akmalhdi <akmalhadi0428@gmail.com>
2026-01-21 10:36:49 +0700

    session 3

--- app/(landing)/components/checkout/order-information.tsx
+++ app/(landing)/components/checkout/order-information.tsx
@@ -1,20 +1,31 @@
+"use client";
 import CardWithHeader from "../ui/card-with-header";
+import { CustomerInfo } from "@/app/hooks/use-cart-store";
 
-const OrderInformation = () => {
+type TOrderInformation = {
+  formData: CustomerInfo;
+  setFormData: React.Dispatch<React.SetStateAction<CustomerInfo>>
+};
+
+const OrderInformation = ({ formData, setFormData }: TOrderInformation) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <CardWithHeader title="Order Information">
       <div className="p-5">
         <div className="input-group">
-          <label htmlFor="full_name">Full Name</label>
-          <input type="text" placeholder="Type your full name" id="full_name" />
+          <label htmlFor="customerName">Full Name</label>
+          <input type="text" placeholder="Type your full name" name="customerName" id="customerName" value={formData.customerName} onChange={handleInputChange} />
         </div>
         <div className="input-group">
-          <label htmlFor="wa_number">Whatsapp Number</label>
-          <input type="text" placeholder="Type your whatsapp number" id="wa_number" />
+          <label htmlFor="customerContact">Whatsapp Number</label>
+          <input type="text" placeholder="Type your whatsapp number" name="customerContact" id="customerContact" value={formData.customerContact ?? ""} onChange={handleInputChange} />
         </div>
         <div className="input-group">
-          <label htmlFor="shipping_address">Shipping Address</label>
-          <textarea placeholder="Example Street, 18, West Jakarta, Indonesia, 66521" id="shipping_address" rows={7} />
+          <label htmlFor="customerAddress">Shipping Address</label>
+          <textarea placeholder="Example Street, 18, West Jakarta, Indonesia, 66521" name="customerAddress" id="customerAddress" value={formData.customerAddress} onChange={handleInputChange} rows={7} />
         </div>
       </div>
     </CardWithHeader>
--- app/(landing)/components/payment/payment-steps.tsx
+++ app/(landing)/components/payment/payment-steps.tsx
@@ -1,17 +1,51 @@
-"use client"
+"use client";
 import priceFormatter from "@/app/utils/price-formatter";
 import CardWithHeader from "../ui/card-with-header";
 import FileUpload from "../ui/file-upload";
 import Button from "../ui/button";
 import { FiCheckCircle } from "react-icons/fi";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useCartStore } from "@/app/hooks/use-cart-store";
+import { transactionCheckout } from "@/app/services/transaction.service";
 
 const PaymentSteps = () => {
-  const {push} = useRouter();
+  const { push } = useRouter();
+  const { items, customerInfo, reset } = useCartStore();
+  const [file, setFile] = useState<File | null>();
 
-  const uploadAndConfirm = () => {
-    push("/order-status/1")
-  }
+  const totalPrice = items.reduce((total, item) => total + item.price * item.qty, 0);
+
+  const handleConfirmPayment = async () => {
+    if (!file) {
+      alert("Please upload your payment receipt!");
+      return;
+    }
+
+    if (!customerInfo) {
+      alert("Customer information is missing, please return to checkout");
+      push("/checkout");
+      return;
+    }
+
+    try {
+      const formData = new FormData();
+      formData.append("customerName", customerInfo.customerName);
+      formData.append("customerContact", customerInfo.customerContact!.toString());
+      formData.append("customerAddress", customerInfo.customerAddress);
+      formData.append("image", file);
+      formData.append("purchasedItems", JSON.stringify(items.map((item) => ({ productId: item._id, qty: item.qty }))));
+      formData.append("totalPayment", totalPrice!.toString());
+
+      const res = await transactionCheckout(formData);
+
+      alert("Transaction created successfully!");
+      reset();
+      push(`/order-status/${res._id}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <CardWithHeader title="Payment Steps">
@@ -27,14 +61,14 @@ const PaymentSteps = () => {
             Upload the payment receipt/screenshot using the <b>'Upload Receipt & Confirm'</b> button below to validate your transaction.
           </li>
         </ol>
-        <FileUpload />
+        <FileUpload onFileSelect={setFile} />
       </div>
       <div className="border-t border-gray-200 p-4">
         <div className="flex justify-between font-semibold">
           <div className="text-sm">Total</div>
           <div className="text-primary text-xs">{priceFormatter(4500000)}</div>
         </div>
-        <Button variant="dark" className="w-full mt-5" onClick={uploadAndConfirm}>
+        <Button variant="dark" className="w-full mt-5" onClick={handleConfirmPayment}>
           <FiCheckCircle /> Upload Receipt & Confirm
         </Button>
       </div>
